94f66c0c62284b46ea87512182dab0258bd19e58
IainLR <iainrobertzon@gmail.com>
2021-06-09 01:48:07 -0500

    constructor challenge solution

--- src/com/oop/iain/robertson/Main.java
+++ src/com/oop/iain/robertson/Main.java
@@ -3,6 +3,14 @@ package com.oop.iain.robertson;
 public class Main {
 
     public static void main(String[] args) {
+        VipCustomer cus1 = new VipCustomer();
+        System.out.println(cus1.getName());
+
+        VipCustomer cus2 = new VipCustomer("Jack", "Jack@email.com");
+        System.out.println(cus2.getName());
+
+
+
 
     }
 }
--- src/com/oop/iain/robertson/VipCustomer.java
+++ src/com/oop/iain/robertson/VipCustomer.java
@@ -1,4 +1,36 @@
 package com.oop.iain.robertson;
 
 public class VipCustomer {
+
+    private String name;
+    private String email;
+    private double creditLimit;
+
+    public VipCustomer(String name, String email, double creditLimit) {
+        this.name = name;
+        this.email = email;
+        this.creditLimit = creditLimit;
+    }
+
+    public VipCustomer() {
+        this("default name", "default@email.com", 400.0);
+        System.out.println("no input constructor");
+    }
+
+    public VipCustomer(String name, String email) {
+        this(name, email, 400.0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
 }
