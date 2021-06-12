63e9925eea240b21e81b06901547c07caba2a76a
IainLR <iainrobertzon@gmail.com>
2021-06-12 18:28:49 -0500

    inheritence

---
+++ src/com/oop/iain/robertson/Dog.java
@@ -0,0 +1,33 @@
+package com.oop.iain.robertson;
+
+public class Dog extends Animal{
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(int size, int weight, String name, int eyes, int legs, int tail, int teeth, String coat) {
+        //super is in reference the inherited constructor from Animal
+        //the superclass
+        //the int 1 represents the existence of body and brain
+        super(1, 1, size, weight, name);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew() {
+        System.out.println("Dog.chew() called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        chew();
+        super.eat();
+    }
+}
--- src/com/oop/iain/robertson/Main.java
+++ src/com/oop/iain/robertson/Main.java
@@ -3,14 +3,14 @@ package com.oop.iain.robertson;
 public class Main {
 
     public static void main(String[] args) {
-        VipCustomer cus1 = new VipCustomer();
-        System.out.println(cus1.getName());
+//        VipCustomer cus1 = new VipCustomer();
+//        System.out.println(cus1.getName());
+//
+//        VipCustomer cus2 = new VipCustomer("Jack", "Jack@email.com");
+//        System.out.println(cus2.getName());
+        Animal animal = new Animal(1, 1, 5, 5, "Animal");
 
-        VipCustomer cus2 = new VipCustomer("Jack", "Jack@email.com");
-        System.out.println(cus2.getName());
-
-
-
-
+        Dog dog = new Dog(8, 20, "Yorkie", 2, 4, 1, 20, "long silky");
+        dog.eat();
     }
 }
